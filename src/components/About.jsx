d848ad12bbb67c4bda66c7743440315a7930dee9
Dharmendra59 <singhdhas9559604443a@gmail.com>
2024-12-01 16:44:21 +0530

    dfg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,28 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
 const About = () => {
+
+      useGSAP(() => {
+            const clipAnimation = gsap.timeline({
+              scrollTrigger: {
+                trigger: "#clip",
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+              },
+            });
+        
+            clipAnimation.to(".mask-clip-path", {
+              width: "100vw",
+              height: "100vh",
+              borderRadius: 0,
+            });
+          });   
   return (
     <div id="about" className='min-h-screen w-screen'>
       <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
@@ -15,6 +37,12 @@ const About = () => {
                         digital and physical, into a unified Play Economy
                   </p>
             </div>
+      </div>
+      <div className='h-dvh w-screen' id='clip'>
+            <div className='mask-clip-path about-image'>
+                  <img src="/img/about.webp" alt="background" className="absolute left-0 top-0 size-full object-cover" />
+            </div>
+
       </div>
     </div>
   )
