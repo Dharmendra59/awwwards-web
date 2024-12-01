559988a96ec6441a797a9b7304f256163687a06e
Dharmendra59 <singhdhas9559604443a@gmail.com>
2024-12-01 16:33:54 +0530

    dfg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const About = () => {
   return (
     <div id="about" className='min-h-screen w-screen'>
-      <h1>About</h1>
+      <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+            <h1 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Zentry</h1>
+
+            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>Disc<b>o</b>ver the World's L<b>a</b>rgest shared Adventure
+            </div>
+            <div className='about-subtext'>
+                  <p>The Game of Games begins-your life, now an epic MMORPG</p>
+                  <p className="text-gray-500">
+                        Zentry unites every player from countless games and platforms, both
+                        digital and physical, into a unified Play Economy
+                  </p>
+            </div>
+      </div>
     </div>
   )
 }
